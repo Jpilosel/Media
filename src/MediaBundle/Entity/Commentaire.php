3f148ead88ff364538c723130085ff38277e48bd
Jpilosel <formation@pilosel.fr>
2016-11-22 17:58:56 +0100

    fin

---
+++ src/MediaBundle/Entity/Commentaire.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Commentaire
+ *
+ * @ORM\Table(name="commentaire")
+ * @ORM\Entity(repositoryClass="MediaBundle\Repository\CommentaireRepository")
+ */
+class Commentaire
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="utilisateur", type="string", length=255)
+     */
+    private $utilisateur = 'Anonyme';
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire", type="text")
+     */
+    private $commentaire;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set utilisateur
+     *
+     * @param string $utilisateur
+     * @return Commentaire
+     */
+    public function setUtilisateur($utilisateur)
+    {
+        $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    /**
+     * Get utilisateur
+     *
+     * @return string
+     */
+    public function getUtilisateur()
+    {
+        return $this->utilisateur;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     * @return Commentaire
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Album", inversedBy="commentaires")
+     * @ORM\JoinColumn(name="album_id", referencedColumnName="id")
+     */
+    private $album;
+
+
+    /**
+     * Set album
+     *
+     * @param \MediaBundle\Entity\Album $album
+     * @return Commentaire
+     */
+    public function setAlbum(\MediaBundle\Entity\Album $album = null)
+    {
+        $this->album = $album;
+
+        return $this;
+    }
+
+    /**
+     * Get album
+     *
+     * @return \MediaBundle\Entity\Album
+     */
+    public function getAlbum()
+    {
+        return $this->album;
+    }
+}
---
+++ src/MediaBundle/Form/AlbumType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MediaBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+class AlbumType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titreAlbum')
+            ->add('artiste')
+            ->add('genre', ChoiceType::class, array(
+                'choices' => array('Hiphop' => 'Hiphop', 'Soul' => 'Soul' , 'Rock' => 'Rock')
+            ))
+            ->add('support', ChoiceType::class, array(
+        'choices' => array('Vinyl' => 'Vinyl' , 'CD' => 'CD' , 'Cassette' => 'Cassette')
+        ))
+        ;
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'MediaBundle\Entity\Album'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'mediabundle_album';
+    }
+
+
+
+
+}
